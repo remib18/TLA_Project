9676e281e44a3eeae4fc08c096a4df6e016ecbd3
Rémi Bernard <remib18@outlook.fr>
2023-12-13 10:53:09 +0100

    Adding base files

---
+++ src/Lieu.java
@@ -0,0 +1,16 @@
+import java.util.List;
+
+/*
+ * Lieu dans l'aventure.
+ * 
+ * Composée d'une description et d'aucune, une ou plusieurs propositions.
+ */
+public class Lieu {
+    String description;
+    List<Proposition> propositions;
+
+    public Lieu(String description, List<Proposition> propositions) {
+        this.description = description;
+        this.propositions = propositions;
+    }
+}

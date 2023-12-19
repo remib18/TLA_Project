ffd084a6bbebb3c99473695666b32bfb3416fceb
Roosewald <118015442+Roosewald@users.noreply.github.com>
2023-12-19 18:32:04 +0100

    correction Register

--- src/App/lexicalAnalysis/LexicalAnalysis.java
+++ src/App/lexicalAnalysis/LexicalAnalysis.java
@@ -16,7 +16,6 @@ public class LexicalAnalysis {
     private Integer state;
     private final List<Token<?>> res;
     private StringBuilder buffer;
-    private boolean insideString = false;
 
     /**
      * Run the analysis
@@ -104,17 +103,8 @@ public class LexicalAnalysis {
 
         switch (symbolIndex) {
             case 101:
-                // On commence à enregistrer une nouvelle chaîne de caractères mais sans enregistrer le token "
-                // Lorsque l'on rencontre un nouveau ", on enregistre le token
-                if (insideString) {
-                    // Si nous sommes à l'intérieur d'une chaîne de caractères, nous enregistrons le token strValue
-                    tokenType = Tokens.strValue;
-                    tokenValue = buffer.toString();
-                    insideString = false;
-                } else {
-                    buffer = new StringBuilder();
-                    insideString = true;
-                }
+                tokenType = Tokens.strValue;
+                tokenValue = buffer.substring(1, buffer.length() - 1);
                 break;
             case 102:
                 String bufferContent = buffer.toString();
